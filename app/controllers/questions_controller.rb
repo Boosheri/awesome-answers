bbd8bc578d1233c3994c1b5abd8ddb74893d7fe0
Nina Boosheri <nboosheri@gmail.com>
2019-04-10 17:17:18 -0700

    create question and answers controllers, models, and views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
 
   def new
     @question = Question.new
@@ -14,14 +15,6 @@ class QuestionsController < ApplicationController
     # # and you're getting the data that you want, is to
     # # render the params as JSON. This is like doing
     # # res.send(req.body) in Express
-
-    # Use 'require' on the params object to retrieve
-    # the nested hash of a key usually corresponding to
-    # the name-value pairs of a form
-
-    # Then use permit to specify all input names that
-    # are allowable (as symbols).
-    question_params = params.require(:question).permit(:title, :body)
     @question = Question.new question_params
     if @question.save
       # The redirect_to method is used for telling the
@@ -35,20 +28,20 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
+    # For the form_with helper
+    @answer = Answer.new
+    # For the list of answers
+    @answers = @question.answers.order(created_at: :desc)
   end
 
   def index
-  @questions = Question.all.order(created_at: :desc)
+    @questions = Question.all.order(created_at: :desc)
   end
 
   def edit
-    @question = Question.find(params[:id])
   end
 
   def update
-    @question = Question.find(params[:id])
-    question_params = params.require(:question).permit(:title, :body)
     if @question.update question_params
       redirect_to question_path(@question.id)
     else
@@ -57,11 +50,24 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    question = Question.find(params[:id])
-    question.destroy
+    @question.destroy
     redirect_to root_path
   end
 
-end
-  
+  private
 
+  def question_params
+    # Use 'require' on the params object to retrieve
+    # the nested hash of a key usually corresponding to
+    # the name-value pairs of a form
+
+    # Then use permit to specify all input names that
+    # are allowable (as symbols).
+    params.require(:question).permit(:title, :body)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+end
